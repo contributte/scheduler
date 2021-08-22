c3af5654f5069961c40514d3318113fe2294c180
Elektro-potkan <git@elektro-potkan.cz>
2021-08-22 11:32:15 +0200

    Tests - Updated to IJob change

--- tests/fixtures/CustomJob.php
+++ tests/fixtures/CustomJob.php
@@ -4,11 +4,12 @@ namespace Tests\Fixtures;
 
 use Contributte\Scheduler\IJob;
 use DateTime;
+use DateTimeInterface;
 
 final class CustomJob implements IJob
 {
 
-	public function isDue(DateTime $dateTime): bool
+	public function isDue(DateTime $dateTime, DateTimeInterface $lastCheck = null): bool
 	{
 		return true;
 	}
